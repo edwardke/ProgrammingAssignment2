c99502bcb1dae59ad091eb644c64dbe48448e224
edwardke <edwardke@mail.gvsu.edu>
2015-01-22 13:53:58 -0600

    cachematrix.R
    ## The goal of this assignment is to use two functions one to create and
    chache a
    ## special matric object and cache it's inverse and the other to
    retrieve the chached
    ## inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The goal of this assignment is to use two functions one to create and chache a 
+## special matric object and cache it's inverse and the other to retrieve the chached 
+## inverse.
 
-## Write a short comment describing this function
+##assign matrix to p
+p <- matrix()
 
-makeCacheMatrix <- function(x = matrix()) {
 
+##This function creates the set up to invert and cache any square matrix
+makeCacheMatrix <- function(x = matrix()){
+     #sets a null inverse matrix 
+     my.matrix.inv <<- NULL
+
+     #caches the input matrix x
+     mymatrix <<- x 
+
+     #this assigns f1 our input matrix x
+     f1 <- function() mymatrix
+
+     #returning a list that will give us our matrix
+     return(list(get.mymatrix=f1))
 }
 
+## running makeCacheMartrix using the square matriz assigned to p to get everything set up
+h <- makeVector(p)
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## this function computes the invserse of the matrix used in makeCacheMatrix using the 
+## functions created there and then caches the inverse matrix
+
+
+cacheSolve <-function(a,...){
+      ## we only want to invert the matrix if it is missing so this checks for that
+      if (is.null(my.matrix.inv)){
+
+      ## chaching out the inverse so it is there for use later
+      my.matrix.inv<<- solve(a$get.mymatrix()) 
+                } 
+                
+      ## returns the inverse
+      return(my.matrix.inv)
 }
+
+cacheSolve(h)
